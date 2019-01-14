8720ec9ebb13f8657b01c72c61ac822d55a04c44
serglider <sergo.mariupol@yandex.ru>
2019-01-14 06:55:55 +0200

    shape transition WIP commit

---
+++ demo/4/app.js
@@ -0,0 +1,68 @@
+const twParams1 = {
+    easing: 'Linear.easeNone',
+    duration: 1000
+};
+const twParams2 = {
+    easing: 'Linear.easeNone',
+    duration: 1000
+};
+const easeNames = [
+    'Elastic',
+    'Back',
+    'Bounce',
+    'SlowMo',
+    'SteppedEase',
+    'Rough',
+    'Circ',
+    'Expo',
+    'Sine',
+    'Power0.',
+    'Power2',
+    'Power4',
+    'Quad',
+    'Cubic',
+    'Quart',
+    'Cubic',
+    'Strong',
+].reduce((acc, name) => {
+    acc.push(`${name}.easeIn`, `${name}.easeOut`, `${name}.easeInOut`);
+    return acc;
+}, []);
+const esList = ['Linear.easeNone'].concat(easeNames);
+
+const gui = new dat.GUI();
+const f1 = gui.addFolder('transition 1');
+f1.add(twParams1, 'duration', 0, 5000).step(50);
+f1.add(twParams1, 'easing', esList);
+const f2 = gui.addFolder('transition 2');
+f2.add(twParams2, 'duration', 0, 5000).step(50);
+f2.add(twParams2, 'easing', esList);
+
+app(onFirstSceneClick, onSecondSceneClick, TweenMax);
+
+function onFirstSceneClick(scene1, scene2) {
+    const trans = PIXI.SceneTransitionManager.createShapeTransition(scene1, scene2, {
+        easing: twParams1.easing,
+        duration: twParams1.duration
+    });
+    scene1.interactive = false;
+    scene1.buttonMode = false;
+    trans.start().then(() => {
+        console.log('++++++');
+        scene1.interactive = true;
+        scene1.buttonMode = true;
+    });
+}
+
+function onSecondSceneClick(scene1, scene2) {
+    const trans = PIXI.SceneTransitionManager.createShapeTransition(scene2, scene1, {
+        easing: twParams2.easing,
+        duration: twParams2.duration
+    });
+    scene2.interactive = false;
+    scene2.buttonMode = false;
+    trans.start().then(() => {
+        scene2.interactive = true;
+        scene2.buttonMode = true;
+    });
+}
--- src/components/createShapeTransition.js
+++ src/components/createShapeTransition.js
@@ -1,37 +1,57 @@
-export default function createShapeTransition(fromView, toView, common = {}, fadeOut = {}, fadeIn = {}) {
-    const {width, height} = fromView;
-    const bounds = [0, 0, width, height];
-    const defaults = {
-        color: 0x000000,
-        sprite: null
-    };
-    const commonOptions = Object.assign({}, defaults, common);
-    const fadeOutOptions = Object.assign({}, commonOptions, fadeOut);
-    const fadeInOptions = Object.assign({}, commonOptions, fadeIn);
-    fadeOutOptions.bounds = fadeInOptions.bounds = bounds;
-    fadeOutOptions.from = {alpha: 0};
-    fadeInOptions.to = {alpha: 0};
+export default function createShapeTransition(fromView, toView, data) {
 
-    const interimContainer = createFadeMask(fromView, bounds, commonOptions.color);
-    const fadeOutTrans = this.createTransition(fromView, interimContainer, [fadeOutOptions]);
-    const fadeInTrans = this.createTransition(interimContainer, toView, [fadeInOptions]);
+    const defaults = {
+        duration: 1,
+        delay: 0,
+        easing: 'Linear.easeNone',
+        path:[
+            550, 100,
+            570, 150,
+            630, 155,
+            585, 195,
+            600, 250,
+            550, 220,
+            500, 250,
+            515, 195,
+            470, 155,
+            530, 150
+        ],
+        scaleFrom: 3,
+        scaleTo: 0.01
+    };
+    const options = Object.assign({}, defaults, data);
+    const mask = new PIXI.Graphics();
+    fromView.parent.addChild(mask);
+    // mask.scale.x = options.scaleFrom;
+    const tweenObj = {
+        x: 10
+    };
+    const vars = {
+        x: 0.01,
+        paused: true,
+        // delay: options.delay,
+        // ease: options.easing,
+        onUpdate: update
+    };
+    const tw = this._tweenLib.to(tweenObj, 1, vars);
 
     return {
         start: () => {
-            return fadeOutTrans.start().then(() => fadeInTrans.start());
+            fromView.mask = mask;
+            return new Promise(resolve => {
+                tw.eventCallback('onComplete', resolve);
+                tw.play();
+            })
         }
     };
-}
 
-function createFadeMask(fromView, bounds, color) {
-    const toIndex = fromView.parent.getChildIndex(fromView);
-    const container = new PIXI.Container();
-    container.visible = false;
-    fromView.parent.addChildAt(container, toIndex + 1);
-    const fadeMask = new PIXI.Graphics();
-    container.addChild(fadeMask);
-    fadeMask.lineStyle(0);
-    fadeMask.beginFill(color);
-    fadeMask.drawRect(...bounds);
-    return container;
+    function update() {
+        console.log(tweenObj.x);
+        mask.scale.set(tweenObj.scale);
+
+        mask.clear();
+        mask.beginFill(0x000000);
+        mask.drawPolygon(options.path);
+
+    }
 }
